8aa4732306bac5a04b22dbb2aac3c3afa6000f15
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-11-30 22:31:06 -0400

    notification drop menu 1st version done

---
+++ src/app/models/notification.model.ts
@@ -0,0 +1,8 @@
+export interface Notification {
+  notificationId: number;
+  userId: number;
+  title: string;
+  message: string;
+  date: string;
+  status: number;
+}
---
+++ src/app/services/user_services/notification.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Notification } from '../../models/notification.model';
+import apiKey from '../apiKey';
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  constructor(private http: HttpClient) {}
+
+  getUserNotifications(id: number) {
+    return this.http.get<Notification[]>(
+      apiKey.api + '/user/' + id + '/notifications'
+    );
+  }
+  getNotificationDetail(userid: number, notifid: number) {
+    return this.http.get<Notification[]>(
+      apiKey.api + '/user/' + userid + '/notifications/' + notifid
+    );
+  }
+  markSeenNotification(userid: number, notifid: number) {
+    return this.http.get<Notification[]>(
+      apiKey.api + '/user/' + userid + '/notifications/' + notifid
+    );
+  }
+}
