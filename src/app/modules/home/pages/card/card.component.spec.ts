79d7f33ca49361e9da8b22c034f53a027bf64266
coriccf <corina.colque.f@gmail.com>
2020-12-01 12:46:48 -0400

    fixing bill date and adding card list

--- src/app/modules/home/pages/card/card.component.spec.ts
+++ src/app/modules/home/pages/card/card.component.spec.ts
@@ -10,7 +10,7 @@ describe('CardComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [ CardComponent ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
--- src/app/modules/home/pages/card/card.component.ts
+++ src/app/modules/home/pages/card/card.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Card} from '../../../../models/card.model';
+import {CardService} from '../../../../services/user_services/card.service';
+import {MatDialog} from '@angular/material/dialog';
+import {ActivatedRoute} from '@angular/router';
+import {ProjectsService} from '../../../../services/user_services/projects.service';
+import {CreateProjectComponent} from '../../../components/dialogs/create-project/create-project.component';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +12,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  listCard: Card[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private service: CardService, private activatedRoute: ActivatedRoute, public dialog: MatDialog) {
   }
 
+  ngOnInit(): void {
+    this.loadlist();
+  }
+
+  loadlist() {
+    this.service.getAllCard().subscribe(data => {
+      this.listCard = data;
+    });
+  }
 }
