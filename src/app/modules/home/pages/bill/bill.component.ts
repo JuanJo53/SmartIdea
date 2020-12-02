a9b8d574d65da1bfa81e0e85bc2f2d8b2b1f8d88
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-01 22:00:39 -0400

    bill table modified

--- src/app/modules/home/pages/bill/bill.component.ts
+++ src/app/modules/home/pages/bill/bill.component.ts
@@ -1,19 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {BillService} from '../../../../services/user_services/bill.service';
-import {Bill} from '../../../../models/bill.model';
+import { BillService } from '../../../../services/user_services/bill.service';
+import { Bill } from '../../../../models/bill.model';
 @Component({
   selector: 'app-bill',
   templateUrl: './bill.component.html',
-  styleUrls: ['./bill.component.css']
+  styleUrls: ['./bill.component.css'],
 })
 export class BillComponent implements OnInit {
   listBill: Bill[];
-  constructor(private service: BillService) { }
+  displayedColumns: string[] = [
+    'Fecha',
+    'Proyecto',
+    'Plan',
+    'Total',
+    'id_card',
+  ];
+  constructor(private service: BillService) {}
   ngOnInit(): void {
     this.loadlist();
   }
-  loadlist(){
-    this.service.getAllBill().subscribe(data => {
+  loadlist() {
+    this.service.getAllBill().subscribe((data) => {
       this.listBill = data;
     });
   }
