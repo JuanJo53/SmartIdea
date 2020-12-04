928f9c8a4f2fdc168a01d60dc6b413b28344f4b7
Juan Jose Fernandez <juanjofer1999@gmail.com>
2020-12-03 23:03:27 -0400

    buy plan for project done to paymentMethod

--- src/app/modules/home/pages/paymentPlanBuy/paymentPlanBuy.component.ts
+++ src/app/modules/home/pages/paymentPlanBuy/paymentPlanBuy.component.ts
@@ -1,41 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import {PaymentPlanService} from '../../../../services/user_services/paymentPlan.service';
-import {PaymentPlan} from '../../../../models/paymentPlan.model';
+import { PaymentPlanService } from '../../../../services/user_services/paymentPlan.service';
+import { PaymentPlan } from '../../../../models/paymentPlan.model';
 import { ActivatedRoute } from '@angular/router';
-import {IProjects} from '../../../../models/projects.model';
-import {ProjectsService} from '../../../../services/user_services/projects.service';
+import { IProjects } from '../../../../models/projects.model';
+import { ProjectsService } from '../../../../services/user_services/projects.service';
 
 @Component({
   selector: 'app-paymentPlanBuy',
   templateUrl: './paymentPlanBuy.component.html',
-  styleUrls: ['./paymentPlanBuy.component.css']
+  styleUrls: ['./paymentPlanBuy.component.css'],
 })
 export class PaymentPlanBuyComponent implements OnInit {
   paymentplan: PaymentPlan;
-  project: IProjects[];
+  project: IProjects;
 
-  constructor(private service: PaymentPlanService,private projectService: ProjectsService , private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private service: PaymentPlanService,
+    private projectService: ProjectsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
   ngOnInit(): void {
     this.loadpaymentplan();
     this.loadproject();
   }
 
-  loadpaymentplan(){
+  loadpaymentplan() {
     const id = this.activatedRoute.snapshot.params.id;
     this.service.getPaymentPlan(id).subscribe(
-      data => {
-        this.paymentplan= data;
+      (data) => {
+        this.paymentplan = data;
+        console.log(data);
       },
-      err => {
+      (err) => {
         console.log(err);
       }
     );
   }
 
-  loadproject(){
+  loadproject() {
     const idpr = this.activatedRoute.snapshot.params.id;
-    this.projectService.getAllProjects().subscribe(data=>{
-      this.project= data;
+    this.projectService.getProject(idpr).subscribe((data) => {
+      console.log(data);
+      this.project = data;
     });
   }
 }
