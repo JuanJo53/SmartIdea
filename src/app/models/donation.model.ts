23eefe56ce58f8765c76a1949620707c6b2a08a5
lr-salinas <laura.salinas.r@ucb.edu.bo>
2021-01-05 23:40:43 -0400

    adding donations in models

---
+++ src/app/models/donation.model.ts
@@ -0,0 +1,11 @@
+export interface Donation{
+  donationId: number;
+  userId: number;
+  cardId: number;
+  projectsId: number;
+  donationDate: string;
+  amount: number;
+  projectTitle: string;
+  projectDescription: string;
+  cardNumber: number;
+}
